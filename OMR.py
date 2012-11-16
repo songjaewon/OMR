75488a52d35e7fcfdefdd10ed4b19ed442f42616
Jaewon Song <songjaewon@kaist.ac.kr>
2012-11-17 02:50:15 +0900

    1st Task Done
    1st Task Done.
    2nd Task has some errors.

--- OMR.py
+++ OMR.py
@@ -4,11 +4,14 @@ import numpy as np
 import math as math
 
 class OMR() :
-    def __init__(self, targetList, endEffectorList):
+    def __init__(self, targetList, endEffectorList, srcEndEffectorList):
         self.targetList = targetList
         self.endEffectorList = endEffectorList   
-        self.jntList, self.effectiveJntDic = self.getJntListAndEffectiveJntDic(self.targetList, self.endEffectorList)
-        self.PI = 3.1415926535897932384626433832795     
+        self.jntList, self.effectiveJntDic = self.getJntListAndEffectiveJntDic(self.endEffectorList)
+        self.srcJntList, self.srcEffectiveJntDic = self.getJntListAndEffectiveJntDic(srcEndEffectorList)
+        self.PI = 3.1415926535897932384626433832795
+        self.prevTargetPosList = None     
+        self.prevJntAngleList = None
         return    
     
     def getLocalAxes(self, eulerRotAngle, isEulerAngle=True):        
@@ -42,8 +45,8 @@ class OMR() :
         mc.select(cl=True)
         return parentJntList
     
-    def getJntListAndEffectiveJntDic(self, targetList, endEffectorList):
-        assert(len(targetList)==len(endEffectorList))
+    def getJntListAndEffectiveJntDic(self, endEffectorList):
+        assert(len(self.targetList)==len(endEffectorList))
         jntList=[]
         effectiveJntDic={}
         for i in range(len(endEffectorList)):
@@ -62,15 +65,8 @@ class OMR() :
             result = Dmax*(posDisplace/(sum(posDisplace*posDisplace)**0.5))            
         return result
     
-    def DampedLeastSquare(self, jntPosList, jntLocalAxesMat, targetPosList, endEffectorPosList, D=0.0, Dmax=2.0, dampingConstant=3.0):
+    def getJacobian(self, jntPosList, jntLocalAxesMat, endEffectorPosList):
         J = np.zeros((len(self.endEffectorList)*3, len(self.jntList)*3))
-        e = np.zeros((len(self.endEffectorList)*3))
-        for i in range(len(self.endEffectorList)):
-            posDisplace = targetPosList[i] - endEffectorPosList[i]
-            posDisplace = self.clampMag(posDisplace, Dmax)
-            e[3*i + 0] = posDisplace[0]
-            e[3*i + 1] = posDisplace[1]
-            e[3*i + 2] = posDisplace[2]        
         for i in range(len(self.endEffectorList)):
             for j in range(len(self.jntList)):
                 if self.jntList[j] in self.effectiveJntDic[self.endEffectorList[i]]:
@@ -83,17 +79,30 @@ class OMR() :
                     for k in range(3):
                         J[3*i+0][3*j+k] = 0
                         J[3*i+1][3*j+k] = 0
-                        J[3*i+2][3*j+k] = 0        
+                        J[3*i+2][3*j+k] = 0  
+        return J
+    
+    def getDisplaceError(self, targetPosList, endEffectorPosList, D=0.0, Dmax=2.0):
+        e = np.zeros((len(self.endEffectorList)*3))
+        for i in range(len(self.endEffectorList)):
+            posDisplace = targetPosList[i] - endEffectorPosList[i]
+            posDisplace = self.clampMag(posDisplace, Dmax)
+            e[3*i + 0] = posDisplace[0]
+            e[3*i + 1] = posDisplace[1]
+            e[3*i + 2] = posDisplace[2]
+        return e        
+    
+    def DampedLeastSquare(self, Jacobian, D=0.0, Dmax=2.0, dampingConstant=3.0):                
+        J = Jacobian
         temp = np.dot(J,J.T)+(dampingConstant*dampingConstant)*np.identity(len(self.endEffectorList)*3,float)
         J_inverse = np.dot(J.T, np.linalg.inv(temp))
-        jointAngles = np.dot(J_inverse, e)               
-        return jointAngles
+        return J_inverse    
     
-    def ikSolver(self):
+    def ikSolver(self, currentTime):
         targetPosMat = np.zeros((len(self.targetList), 3))
         endEffectorPosMat = np.zeros((len(self.targetList), 3))
         jntPosMat = np.zeros((len(self.jntList), 3))
-        jntAngleMat = np.zeros((len(self.jntList), 3))
+        jntAngleMat = np.zeros((len(self.jntList), 3))        
         for i in range(len(self.targetList)):
             targetPosMat[i] = np.array(mc.xform(self.targetList[i], q=True, ws=True, t=True))
             endEffectorPosMat[i] = np.array(mc.xform(self.endEffectorList[i], q=True, ws=True, t=True))
@@ -103,16 +112,59 @@ class OMR() :
         jntLocalAxesMat = np.zeros((len(self.jntList),3, 3))       
         for i in range(len(self.jntList)):
             jntLocalAxesMat[i] = self.getLocalAxes(jntAngleMat[i])
-        jntAngles = self.DampedLeastSquare(jntPosMat, jntLocalAxesMat, targetPosMat, endEffectorPosMat) 
-        for i in range(len(self.jntList)):
+             
+        J = self.getJacobian(jntPosMat, jntLocalAxesMat, endEffectorPosMat)
+        J_inverse = self.DampedLeastSquare(J)       
+        if currentTime == 1 :
+            self.prevTargetPosList = targetPosMat
+            displaceTargetPos = self.getDisplaceError(targetPosMat, endEffectorPosMat)            
+        else :            
+            displaceTargetPos = self.getDisplaceError(self.prevTargetPosList, targetPosMat)
+            jntAngles =  np.dot(J_inverse, displaceTargetPos)
+            #to get fk funtion value f(theta). we need proper way. 
             degX = jntAngles[3*i]*(180/self.PI)
             degY = jntAngles[3*i+1]*(180/self.PI)
             degZ = jntAngles[3*i+2]*(180/self.PI)
             mc.xform(self.jntList[i], eu=True, r=True, ro=[degX, degY, degZ])
+            tempTargetPosMat = np.zeros((len(self.targetList), 3))
+            for i in range(len(self.targetList)):
+                tempTargetPosMat[i] = np.array(mc.xform(self.endEffectorList[i], q=True, ws=True, t=True))
+            e = self.getDisplaceError(targetPosMat, tempTargetPosMat)
+            displaceTargetPos = displaceTargetPos+e            
+            mc.xform(self.jntList[i], eu=True, r=True, ro=[-degX, -degY, -degZ])
+            self.prevTargetPosList = targetPosMat        
+        
+        srcJntAngles = np.zeros((len(self.srcJntList*3)))
+        for i in range(len(self.srcJntList)):
+            rot = mc.xform(self.srcJntList[i], eu=True, ro=True, r=True, q=True)
+            srcJntAngles[3*i] = rot[0]*(self.PI/180.0)
+            srcJntAngles[3*i+1] = rot[1]*(self.PI/180.0)
+            srcJntAngles[3*i+2] = rot[2]*(self.PI/180.0)
+                
+        tempMat = np.identity(len(self.jntList)*3,float) - np.dot(J_inverse, J)
+        
+        if currentTime == 1:
+            secondaryJntAngles = np.dot(tempMat, srcJntAngles - np.dot(J_inverse,self.getDisplaceError(targetPosMat, endEffectorPosMat)))
+        else :
+            secondaryJntAngles = np.dot(tempMat, srcJntAngles - (self.prevJntAngleList + np.dot(J_inverse,self.getDisplaceError(self.prevTargetPosList, targetPosMat))))              
+        
+                
+        jntAngles =  np.dot(J_inverse, displaceTargetPos) 
+        self.prevJntAngleList = jntAngles 
+        
+        for i in range(len(self.jntList)):
+            degX = jntAngles[3*i]*(180.0/self.PI)
+            degY = jntAngles[3*i+1]*(180.0/self.PI)
+            degZ = jntAngles[3*i+2]*(180.0/self.PI)
+            mc.xform(self.jntList[i], eu=True, r=True, ro=[degX, degY, degZ])        
+            mc.setKeyframe(['joint7','joint8','joint9','joint10','joint11','joint12'])
 
+a = OMR(['locator1'],['joint12'], ['joint6'])
+#a = OMR(['locator1','locator2','locator3','locator4'],['joint5','joint9','joint13','joint17'])
 
-#a = OMR(['locator1', 'locator2', 'locator3'],['joint4', 'joint7', 'joint10'])
-a = OMR(['locator1','locator2','locator3','locator4'],['joint5','joint9','joint13','joint17'])
 #a.ikSolver(['pSphere1', 'pSphere2'], [])
-a.ikSolver()
+a.ikSolver(1)
+for i in range(200):
+    mc.currentTime(i+1)
+    a.ikSolver(mc.currentTime(q=True))    
 
